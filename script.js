0f039b41fb04de3e21c5666b28f002cc8e57fb8d
Creva-solution <crevasolution@gmail.com>
2025-10-12 21:45:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -65,3 +65,9 @@ if (contactForm) {
     }
   });
 }
+// google analytics
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-QMSB2QLQJX');
+
